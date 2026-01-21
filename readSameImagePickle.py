65b1e021839e51d2adcefe1e3d0f7035ebcca21b
BlueSilverCat <bluesilvercat@gmail.com>
2026-01-21 12:00:17 +0900

    add sameImages count

--- readSameImagePickle.py
+++ readSameImagePickle.py
@@ -8,8 +8,9 @@ def argumentParser():
   parser = argparse.ArgumentParser()
   parser.add_argument("path", type=pathlib.Path)
   parser.add_argument("-k", "--sortKey", choices=["path", "count"], default="path")
+  parser.add_argument("-v", "--verbose", action="store_true")
   args = parser.parse_args()
-  return args.path, args.sortKey
+  return args.path, args.sortKey, args.verbose
 
 
 def printSameImagePickle(path, sortKey="path", *, isVerbose=False):
@@ -20,30 +21,34 @@ def printSameImagePickle(path, sortKey="path", *, isVerbose=False):
   pm = U.PickleManager(path)
   data = pm.loadExternal()
   directory = data.pop(0)
+  target = data.pop(0)
+  extensions = data.pop(0)
+  dirs = data.pop()
   print(f"directory:   {directory}")
-  print(f"same images: {pm.count - 1}")
-  print(f"total:       {sum(map(len, data))}")
-  if pm.count < 2:  # noqa: PLR2004
-    return
-  counter = {}
-  for lt in data:
-    for dt in lt:
-      U.countDict(counter, str(U.subPath(dt["path"].parent, directory)))
-  width = max(len(str(v)) for v in counter.values())
+  print(f"target:      {target}")
+  print(f"extensions:  {extensions}")
+  wt = max(len(str(v["total"])) for v in dirs.values())
+  ws = max(len(str(v["sames"])) for v in dirs.values())
+  print("dirs:")
   dt = (
-    sorted(counter.items(), key=lambda x: x[1], reverse=True)
+    sorted(filter(lambda x: x[1]["sames"] > 0, dirs.items()), key=lambda x: x[1]["sames"], reverse=True)
     if sortKey == "count"
-    else sorted(counter.items(), key=lambda x: x[0], reverse=False)
+    else sorted(filter(lambda x: x[1]["sames"] > 0, dirs.items()), key=lambda x: x[0], reverse=False)
   )
   for k, v in dt:
-    print(f"{v:{width}}: {k}")
+    print(f"{v['sames']:{wt}} / {v['total']:{ws}}: {U.subPath(k, directory)}")
+  print(f"same images: {pm.count - 1}")
+  print(f"total:       {sum(map(len, data))}")
 
   if isVerbose:
     for lt in data:
+      n = len(lt)
+      print(n)
       for dt in lt:
-        print(dt)
+        diff = str(dt.get("target", "")) + str(dt.get("diff", ""))
+        print(f"{U.subPath(dt['path'], directory)}, {dt['shape']}, {diff}")
 
 
 if __name__ == "__main__":
-  path, sortKey = argumentParser()
-  printSameImagePickle(path, sortKey)
+  path, sortKey, isVerbose = argumentParser()
+  printSameImagePickle(path, sortKey, isVerbose=isVerbose)
